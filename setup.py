563f8853cef86a6c63555dd5b195618d1e4306ef
helgeerbe <helge@erbehome.de>
2021-03-10 20:58:33 +0100

    change setup.py to production

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(name='picframe',
       long_description=long_description,
       long_description_content_type='text/markdown',
       classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.7',
         'Topic :: Multimedia :: Graphics :: Viewers',
