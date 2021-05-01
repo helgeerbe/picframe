82bf1363b75d702f8af5a58565a4881bc091cda8
paddywwoof <patrick@eldwick.org.uk>
2021-05-01 08:30:08 +0100

    require pi3d v2.45 to stop image resizing i.e. 4k

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name='picframe',
       install_requires=[
         'Pillow',
         'ExifRead',
-        'pi3d>=2.44',
+        'pi3d>=2.45',
         'PyYAML',
         'paho-mqtt',
         'IPTCInfo3',
