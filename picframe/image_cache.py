0b08dfbb3db1e36aefdc7643a869b2d2ea580cfd
helge <helge@erbehome.de>
2021-05-03 11:12:32 +0200

    Revert "Merge pull request #111 from helgeerbe/Issue-110"
    This reverts commit 882082ae3364e754f9c4439089d2a2ae7441d187, reversing
    changes made to 19eb16225ec1316a439619eb9402835c44d45fbc.

--- picframe/image_cache.py
+++ picframe/image_cache.py
@@ -331,10 +331,7 @@ class ImageCache:
 
     def __insert_file(self, file):
         file_insert = "INSERT OR REPLACE INTO file(folder_id, basename, extension, last_modified) VALUES((SELECT folder_id from folder where name = ?), ?, ?, ?)"
-        # Insert the new folder if it's not already in the table. Update the missing field separately.
-        folder_insert = "INSERT INTO folder(name) SELECT ? WHERE NOT EXISTS(SELECT 1 FROM folder WHERE name = ?)"
-        folder_update = "UPDATE folder SET missing = 0 where name = ?"
-
+        folder_insert = "INSERT OR REPLACE INTO folder(name, missing) VALUES(?, 0)"
         mod_tm =  os.path.getmtime(file)
         dir, file_only = os.path.split(file)
         base, extension = os.path.splitext(file_only)
@@ -346,8 +343,7 @@ class ImageCache:
         vals.insert(0, file)
 
         # Insert this file's info into the folder, file, and meta tables
-        self.__db.execute(folder_insert, (dir,dir))
-        self.__db.execute(folder_update, (dir,))
+        self.__db.execute(folder_insert, (dir,))
         self.__db.execute(file_insert, (dir, base, extension.lstrip("."), mod_tm))
         self.__db.execute(meta_insert, vals)
 
