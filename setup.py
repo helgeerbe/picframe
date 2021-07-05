71c8efb6a9018b86d86c35ebbf9846818c057934
Helge <helge@erbehome.de>
2021-07-05 09:43:10 +0200

    Update dependency of pi3d to 2.46 to solve #131

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name='picframe',
       install_requires=[
         'Pillow',
         'ExifRead',
-        'pi3d>=2.45',
+        'pi3d>=2.46',
         'PyYAML',
         'paho-mqtt',
         'IPTCInfo3',
