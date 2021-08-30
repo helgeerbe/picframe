ac0c85cc32dfa67ba67b38749a382e741d1f7ea7
helgeerbe <helge@erbehome.de>
2021-08-30 10:14:45 +0200

    bump pi3d v2.47

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(name='picframe',
       install_requires=[
         'Pillow',
         'ExifRead',
-        'pi3d>=2.46',
+        'pi3d>=2.47',
         'PyYAML',
         'paho-mqtt',
         'IPTCInfo3',
