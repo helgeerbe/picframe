8a7da7086bf630aab783b56d2ccb202fd6af73ff
helgeerbe <helge@erbehome.de>
2022-12-19 13:41:26 +0100

    flake8 get_image_meta.py

--- src/picframe/get_image_meta.py
+++ src/picframe/get_image_meta.py
@@ -1,7 +1,5 @@
 import logging
-import os
 from PIL import Image
-from PIL.Image import Exif
 from PIL.ExifTags import TAGS, GPSTAGS
 from pi_heif import register_heif_opener
 from fractions import Fraction
@@ -9,12 +7,13 @@ from fractions import Fraction
 
 register_heif_opener()
 
+
 class GetImageMeta:
 
     def __init__(self, filename):
         self.__logger = logging.getLogger("get_image_meta.GetImageMeta")
         self.__tags = {}
-        self.__filename = filename # in case no exif data in which case needed for size
+        self.__filename = filename  # in case no exif data in which case needed for size
         image = self.get_image_object(filename)
         if image:
             exif = image.getexif()
@@ -29,10 +28,9 @@ class GetImageMeta:
             except Exception as e:
                 xmp = {}
                 self.__logger.warning("PILL getxmp() failed: %s -> %s", filename, e)
-                
 
     def __do_image_tags(self, exif):
-        tags =  {
+        tags = {
             "Image " + str(TAGS.get(key, key)): value
             for key, value in exif.items()
         }
@@ -43,8 +41,8 @@ class GetImageMeta:
             if value == "ExifOffset":
                 break
         info = exif.get_ifd(key)
-        tags =  {
-            "EXIF " +  str(TAGS.get(key, key)): value
+        tags = {
+            "EXIF " + str(TAGS.get(key, key)): value
             for key, value in info.items()
         }
         self.__tags.update(tags)
@@ -54,28 +52,28 @@ class GetImageMeta:
             if value == "GPSInfo":
                 break
         gps_info = exif.get_ifd(key)
-        tags =  {
+        tags = {
             "GPS " + str(GPSTAGS.get(key, key)): value
             for key, value in gps_info.items()
         }
         self.__tags.update(tags)
-    
+
     def __find_xmp_key(self, key, dic):
         for k, v in dic.items():
             if key == k:
                 return v
             elif isinstance(v, dict):
-                val = self.__find_xmp_key(key, v) 
+                val = self.__find_xmp_key(key, v)
                 if val:
-                    return val 
+                    return val
             elif isinstance(v, list):
                 for x in v:
                     if isinstance(x, dict):
-                        val = self.__find_xmp_key(key, x) 
+                        val = self.__find_xmp_key(key, x)
                         if val:
-                            return val 
+                            return val
         return None
-    
+
     def __do_xmp_keywords(self, xmp):
         try:
             # title
@@ -92,15 +90,15 @@ class GetImageMeta:
             except KeyError:
                 pass
             # tags
-            try: 
+            try:
                 val = self.__find_xmp_key('subject', xmp)
                 if val:
                     val = val['Bag']['li']
                     if val and isinstance(val, list) and len(val) > 0:
                         tags = ''
                         for tag in val:
-                            tags += tag +  ","
-                        self.__tags['IPTC Keywords'] = tags 
+                            tags += tag + ","
+                        self.__tags['IPTC Keywords'] = tags
             except KeyError:
                 pass
         except Exception as e:
@@ -109,16 +107,16 @@ class GetImageMeta:
     def __do_iptc_keywords(self):
         try:
             from iptcinfo3 import IPTCInfo
-            iptcinfo_logger = logging.getLogger('iptcinfo') # turn off useless log infos
+            iptcinfo_logger = logging.getLogger('iptcinfo')  # turn off useless log infos
             iptcinfo_logger.setLevel(logging.ERROR)
             with open(self.__filename, 'rb') as fh:
-                iptc = IPTCInfo(fh, force=True, out_charset='utf-8') # TODO put IPTC read in separate function
+                iptc = IPTCInfo(fh, force=True, out_charset='utf-8')  # TODO put IPTC read in separate function
                 # tags
                 val = iptc['keywords']
                 if val is not None and len(val) > 0:
                     keywords = ''
                     for key in iptc['keywords']:
-                        keywords += key.decode('utf-8')  + ','  # decode binary strings
+                        keywords += key.decode('utf-8') + ','  # decode binary strings
                     self.__tags['IPTC Keywords'] = keywords
                 # caption
                 val = iptc['caption/abstract']
@@ -129,7 +127,7 @@ class GetImageMeta:
                 if val is not None and len(val) > 0:
                     self.__tags['IPTC Object Name'] = iptc['object name'].decode('utf-8')
         except Exception as e:
-            self.__logger.warning("IPTC loading has failed - if you want to use this you will need to install iptcinfo3 %s -> %s",
+            self.__logger.warning("IPTC loading has failed - if you want to use this you will need to install iptcinfo3 %s -> %s",  # noqa: E501
                                   self.__filename, e)
 
     def has_exif(self):
@@ -185,7 +183,8 @@ class GetImageMeta:
 
     def get_exif(self, key):
         try:
-            iso_keys = ['EXIF ISOSpeedRatings', 'EXIF PhotographicSensitivity', 'EXIF ISO'] # ISO prior 2.2, ISOSpeedRatings 2.2, PhotographicSensitivity 2.3
+            # ISO prior 2.2, ISOSpeedRatings 2.2, PhotographicSensitivity 2.3
+            iso_keys = ['EXIF ISOSpeedRatings', 'EXIF PhotographicSensitivity', 'EXIF ISO']
             if key in iso_keys:
                 for iso in iso_keys:
                     val = self.__get_if_exist(iso)
@@ -215,7 +214,7 @@ class GetImageMeta:
             return None
 
     def get_size(self):
-        try: # corrupt image file might crash app
+        try:  # corrupt image file might crash app
             return GetImageMeta.get_image_object(self.__filename).size
         except Exception as e:
             self.__logger.warning("get_size failed on %s -> %s", self.__filename, e)
@@ -223,12 +222,11 @@ class GetImageMeta:
 
     @staticmethod
     def get_image_object(fname):
-        ext = os.path.splitext(fname)[1].lower()
         try:
             image = Image.open(fname)
-            if image.mode not in ("RGB", "RGBA"): # mat system needs RGB or more
+            if image.mode not in ("RGB", "RGBA"):  # mat system needs RGB or more
                 image = image.convert("RGB")
-        #raise # the system should be able to withstand files being moved etc without crashing
+        # raise # the system should be able to withstand files being moved etc without crashing
         except Exception as e:
             logger = logging.getLogger("get_image_meta.GetImageMeta")
             logger.warning("Can't open file: \"%s\"", fname)
