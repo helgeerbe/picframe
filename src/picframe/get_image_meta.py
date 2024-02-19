9d50b20dcee87f3b7b18b464b72b103ae89bdb5b
helgeerbe <helge@erbehome.de>
2024-02-19 10:54:15 +0100

    Revert "Merge commit 'f4f034e2a37b4f5f880823ab5abb3004af126626' into dev"
    This reverts commit 5173ce9d0159cf4b2b66b35ef33c096a21f10f47, reversing
    changes made to c47ede48e74ae5c0495f3aef5deb6a04cf773710.

--- src/picframe/get_image_meta.py
+++ src/picframe/get_image_meta.py
@@ -192,9 +192,6 @@ class GetImageMeta:
                 for iso in iso_keys:
                     val = self.__get_if_exist(iso)
                     if val:
-                        # If ISO is returned as a tuple, take the first element
-                        if type(val) is tuple:
-                            val = val[0]
                         break
             else:
                 val = self.__get_if_exist(key)
