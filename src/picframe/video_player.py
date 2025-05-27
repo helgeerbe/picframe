6b98c1d4a55ad0cbf4922c106fe3ef2e91b0609d
helgeerbe <helge@erbehome.de>
2025-05-27 11:01:23 +0200

    move cursor only if window becomes visable

--- src/picframe/video_player.py
+++ src/picframe/video_player.py
@@ -169,8 +169,8 @@ class VideoPlayer:
                             self.logger.warning("Player window not shown within %d seconds.", timeout)
                         else:
                             time.sleep(0.1)  # Give the compositor a moment to actually draw the window
-                    sdl2.SDL_ShowCursor(sdl2.SDL_DISABLE)
-                    sdl2.SDL_WarpMouseInWindow(self.window, self.w - 1, self.h - 1)
+                        sdl2.SDL_ShowCursor(sdl2.SDL_DISABLE)
+                        sdl2.SDL_WarpMouseInWindow(self.window, self.w - 1, self.h - 1)
                     self._send_state("PLAYING")
                 elif state in [vlc.State.Opening,
                                vlc.State.Buffering,
