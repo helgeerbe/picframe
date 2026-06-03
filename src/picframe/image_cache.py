9602c906a003e5b4dbc686e21a928f8c23a25d7e
Shuaib Munshi <munshi.shuaib@gmail.com>
2026-06-02 21:37:43 -0400

    handle tif and bmp

--- src/picframe/image_cache.py
+++ src/picframe/image_cache.py
@@ -9,7 +9,7 @@ from picframe.video_streamer import VIDEO_EXTENSIONS, get_video_info
 
 class ImageCache:
 
-    EXTENSIONS = ['.png', '.jpg', '.jpeg', '.heif', '.heic']
+    EXTENSIONS = ['.png', '.jpg', '.jpeg', '.heif', '.heic', '.tif', '.tiff', '.bmp']
     EXIF_TO_FIELD = {'EXIF FNumber': 'f_number',
                      'Image Make': 'make',
                      'Image Model': 'model',
