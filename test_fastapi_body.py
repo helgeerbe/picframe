7d78048b18d1fa30262abf69571110453bdd5fdc
helgeerbe <helge@erbehome.de>
2026-06-07 21:27:38 +0000

    Remove root debug test scripts

--- test_fastapi_body.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from picframe.api.models import AppConfig
-app = FastAPI()
-@app.put("/test")
-def test(payload: AppConfig):
-    return "ok"
-print(app.openapi()["paths"]["/test"]["put"])
